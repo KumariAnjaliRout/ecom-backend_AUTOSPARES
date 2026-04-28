2d3802f414f53c6160eca3bd027ec8f857b321bd
KumariAnjaliRout <151063318+KumariAnjaliRout@users.noreply.github.com>
2026-04-28 12:20:04 +0530

    fix: read Firebase from env variable

--- src/main/java/com/automobile/ecom/config/FirebaseConfig.java
+++ src/main/java/com/automobile/ecom/config/FirebaseConfig.java
@@ -1,33 +1,26 @@
 package com.automobile.ecom.config;
 
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-
-        InputStream serviceAccount =
-                getClass().getClassLoader()
-                        .getResourceAsStream("firebase-service-account.json");
-
-        if (serviceAccount == null) {
-            throw new RuntimeException("Firebase service account file not found");
+        String credentials = System.getenv("FIREBASE_CREDENTIALS");
+        if (credentials == null) {
+            throw new RuntimeException("FIREBASE_CREDENTIALS env variable not set");
         }
-
+        ByteArrayInputStream stream = new ByteArrayInputStream(credentials.getBytes());
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(stream))
                 .build();
-
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
         }
